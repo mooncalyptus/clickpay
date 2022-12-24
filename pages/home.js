8513715513585b32e9a7067758d914b89054e917
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2022-12-25 00:08:08 +0700

    create home and reusable component navbar

---
+++ pages/home.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import Image from "next/image";
+import Navbar from "./navbar";
+
+const Home = () => {
+return(
+    <>
+    <Navbar></Navbar>
+    <div className="flex font-nunito">
+        <section className="bg-[#E5E5E5]">
+
+            <div className="bg-white ml-[20%]">
+                <div className="flex gap-[23px]">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" /></svg>
+                <span className="text-[#82C3EC] text-lg font-bold">Dashboard</span>
+                </div>
+
+                <div className="flex gap-[23px]">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" /></svg>
+                <span className="text-[#3A3D42] opacity-80 text-lg font-bold">Transfer</span>
+                </div>
+
+                <div className="flex gap-[23px]">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>
+                <span className="text-[#3A3D42] opacity-80 text-lg font-bold">Top Up</span>
+                </div>
+
+                <div className="flex gap-[23px]">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
+                <span className="text-[#3A3D42] opacity-80 text-lg font-bold">Profile</span>
+                </div>
+
+                <div className="flex gap-[23px] pt-[268px]">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" /></svg>
+                <span className="text-[#3A3D42] opacity-80 text-lg font-bold">Logout</span>
+                </div>
+            </div>
+        </section>
+        <section>Section isi</section>
+    </div>
+    </>
+)
+}
+
+export default Home
