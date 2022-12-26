f92c9318340dfa8cbc16533e3aded0a7c9702ecb
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2022-12-26 19:25:00 +0700

    created dropdown notification on navbar and profile page

--- pages/navbar.js
+++ pages/navbar.js
@@ -2,28 +2,90 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Navbar = () => {
-return(
-    <>
-    <div className="flex bg-[#82C3EC] font-nunito">
-        <div className="grow">
-        <Image src={require('../assets/logo-remove.png')} className="ml-[5%]" width="150" height="40" alt="desc" ></Image>
-        </div>
+    return (
+        <>
+            <div className="flex bg-[#82C3EC] font-nunito">
+                <div className="grow">
+                    <Image src={require('../assets/logo-remove.png')} className="ml-[5%]" width="150" height="40" alt="desc" ></Image>
+                </div>
 
-        <div className="flex justify-center items-center gap-5 mr-[10%]">
-            <div>
-            <Image src={require('../assets/profile.png')} alt="desc" ></Image>
+                <div className="flex justify-center items-center gap-5 mr-[10%]">
+                    <div>
+                        <Image src={require('../assets/profile.png')} alt="desc" ></Image>
+                    </div>
+                    <div className="flex flex-col">
+                        <span className="text-lg font-bold">Robert Chandler</span>
+                        <span className="text-[13px] text-[#3A3D42] opacity-90">+62 8139 3877 7946</span>
+                    </div>
+                    <div>
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0}>
+                                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-[400px] px-[3opx] gap-5">
+                                <li>
+                                    <div className="flex gap-6">
+                                        <div><svg className="w-6 h-6 text-[#4CEDB3]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" /></svg></div>
+
+                                        <div>
+                                            <div className="text-sm text-[#7A7A7A]">Accept from Joshua Lee</div>
+                                            <div className="text-lg font-bold">Rp220.000</div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div className="flex gap-6">
+                                        <div><svg className="w-6 h-6 text-[#FF5B37]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" /></svg></div>
+
+                                        <div>
+                                            <div className="text-sm text-[#7A7A7A]">Transfer to Deni</div>
+                                            <div className="text-lg font-bold">Rp149.000</div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div className="flex gap-6">
+                                        <div><svg className="w-6 h-6 text-[#FF5B37]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" /></svg></div>
+
+                                        <div>
+                                            <div className="text-sm text-[#7A7A7A]">Transfer to Deni</div>
+                                            <div className="text-lg font-bold">Rp149.000</div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div className="flex gap-6">
+                                        <div><svg className="w-6 h-6 text-[#FF5B37]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" /></svg></div>
+
+                                        <div>
+                                            <div className="text-sm text-[#7A7A7A]">Transfer to Jessice Lee</div>
+                                            <div className="text-lg font-bold">Rp100.000</div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <div className="flex gap-6">
+                                        <div><svg className="w-6 h-6 text-[#4CEDB3]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" /></svg></div>
+
+                                        <div>
+                                            <div className="text-sm text-[#7A7A7A]">Top Up</div>
+                                            <div className="text-lg font-bold">Rp300.000</div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+                        {/*  */}
+                    </div>
+                </div>
             </div>
-            <div className="flex flex-col">
-                <span className="text-lg font-bold">Robert Chandler</span>
-                <span className="text-[13px] text-[#3A3D42] opacity-90">+62 8139 3877 7946</span>
-            </div>
-            <div>
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-            </div>
-        </div>
-    </div>
-    </>
-)
+        </>
+    )
 }
 
 export default Navbar
---
+++ pages/profile.js
@@ -0,0 +1,99 @@
+import Link from "next/link";
+import Image from "next/image";
+import Navbar from "./navbar";
+import FooterUser from "./footer-user";
+import ModalTopUp from "./modal-topup";
+import { useState } from "react";
+
+const Profile = () => {
+    const [showModal, setShowModal] = useState(false)
+    return (
+        <>
+            <Navbar></Navbar>
+
+            <div className="flex bg-[#E5E5E5] pt-5 pb-[35px]">
+                {/* Dashboard kiri */}
+                <div className="bg-white rounded-md ml-[5%] pl-[3%] pr-[5%] pt-[47px]">
+                    <div className="h-screen">
+                        <div className="flex gap-x-5">
+                            <svg className="w-6 h-6 text-[#3A3D42] opacity-80" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" /></svg>
+                            <span className="text-[#3A3D42] opacity-80">Dashboard</span>
+                        </div>
+
+                        <div className="flex gap-x-5 mt-[52px]">
+                            <svg className="w-6 h-6 text-[#3A3D42] opacity-80" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" /></svg>
+                            <span className="text-[#3A3D42] opacity-80">Transfer</span>
+                        </div>
+
+                        <div className="flex gap-x-5 mt-[52px]">
+                            <svg className="w-6 h-6 text-[#3A3D42] opacity-80" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>
+                            <span className="text-[#3A3D42] opacity-80">Top Up</span>
+                        </div>
+
+                        <div className="flex gap-x-5 mt-[52px]">
+                            <svg className="w-6 h-6 text-[#82C3EC]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
+                            <span className="text-lg font-bold text-[#82C3EC]">Profile</span>
+                        </div>
+                    </div>
+
+                    <div className="flex gap-5 mb-[10%]">
+                        <svg className="w-6 h-6 text-[#3A3D42] opacity-80" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" /></svg>
+                        <span className="text-[#3A3D42] opacity-80">Logout</span>
+                    </div>
+                </div>
+
+                {/* Konten kanan */}
+                <div className="ml-5 mr-[5%] w-full bg-white rounded-md">
+                    <div className="flex flex-col justify-center items-center pt-6 gap-[10px]">
+                        <div>
+                            <Image src={require('../assets/profile.png')} alt="desc" ></Image>
+                        </div>
+
+                        <div className="flex">
+                            <div><svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" /></svg></div>
+                            <div>Edit</div>
+                        </div>
+
+                        <div>Robert Chandler</div>
+
+                        <div>+62 813-9387-7946</div>
+
+                        <div className="flex flex-col gap-5">
+                            <button className="flex bg-[#82C3EC] py-[18px] px-5 w-64 rounded-lg">
+                                    <div className="grow">Personal Information</div>
+                                <div>
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" /></svg>
+                                </div>
+                            </button>
+
+                            <button className="flex bg-[#82C3EC] py-[18px] px-5 w-64 rounded-lg">
+                                    <div className="grow">Change Password</div>
+                                <div>
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" /></svg>
+                                </div>
+                            </button>
+
+                            <button className="flex bg-[#82C3EC] py-[18px] px-5 w-64 rounded-lg">
+                                    <div className="grow">Change PIN</div>
+                                <div>
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" /></svg>
+                                </div>
+                            </button>
+
+                            <button className="flex bg-[#82C3EC] py-[18px] px-5 w-64 rounded-lg">
+                                    <div className="grow">Logout</div>
+                                <div>
+                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" /></svg>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <FooterUser></FooterUser>
+        </>
+    )
+}
+
+export default Profile
